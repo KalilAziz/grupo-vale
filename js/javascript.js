81c5851f76856631daec3c1c127cc656e5ce7145
Kalil Aziz Santos Chami <kalilazizsantoschami@hotmail.com>
2022-02-20 13:28:34 -0300

    Index e sobrenos

--- js/javascript.js
+++ js/javascript.js
@@ -1,50 +1,59 @@
 window.onload = function(){
-    document.querySelector("#menu1 ul").style.display = 'none'
-    document.querySelector(".menuMobile").addEventListener("click", function(){
-        
-        if(document.querySelector("#menu1 ul").style.display == 'none'){
-            document.querySelector("#menu1 ul").style.display = 'block'
-        } else if(document.querySelector("#menu1 ul").style.display = 'block'){
-            document.querySelector("#menu1 ul").style.display = 'none'
-        }
-    })
+  document.querySelector("#menu1 ul").style.display = 'none'
+  document.querySelector(".menuMobile").addEventListener("click", function(){
+      
+      if(document.querySelector("#menu1 ul").style.display == 'none'){
+          document.querySelector("#menu1 ul").style.display = 'block'
+      } else if(document.querySelector("#menu1 ul").style.display = 'block'){
+          document.querySelector("#menu1 ul").style.display = 'none'
+      }
+  })
 
-    document.querySelector("#area").addEventListener("click", function(){
-        if(document.querySelector(".most").style.display == 'none'){
-            document.querySelector(".most").style.display = 'block'
-            document.querySelector(".most").style.textAlign = 'left'
-            document.querySelector(".most").style.margin = '0px 0px 0px 65px'
-            document.querySelector("#area").style.fontSize = '1.2rem'
-            document.querySelector("#area").style.color = 'red'
-        }
-        else{
-            document.querySelector(".most").style.display = 'none'
-            document.querySelector("#area").style.color = 'white'
-            document.querySelector("#area").style.fontSize = '1rem'
-        }
-    })
-    document.querySelector("#items")
-    .addEventListener("wheel", event =>{
-         if(event.deltaY < 0){
-             event.target.scrollBy(-300, 0)
-         }else{
-            event.target.scrollBy(300, 0)
-         }
-    })
-
-    document.querySelector("#up").addEventListener("click", function(){
-        
-         document.querySelector("#items").addEventListener("wheel", event =>{
-         if(event.deltaY < 0){
-             event.target.scrollBy(300, 0)
-         }else{
-            event.target.scrollBy(-300, 0)
-         }
-    }) 
-    
+  document.querySelector("#area").addEventListener("click", function(){
+      if(document.querySelector(".most").style.display == 'none'){
+          document.querySelector(".most").style.display = 'block'
+          document.querySelector(".most").style.textAlign = 'left'
+          document.querySelector(".most").style.margin = '0px 0px 0px 65px'
+          document.querySelector("#area").style.fontSize = '1.2rem'
+          document.querySelector("#area").style.color = 'red'
+      }
+      else{
+          document.querySelector(".most").style.display = 'none'
+          document.querySelector("#area").style.color = 'white'
+          document.querySelector("#area").style.fontSize = '1rem'
+      }
+  })
+  document.querySelector("#items")
+  .addEventListener("wheel", event =>{
+       if(event.deltaY < 0){
+           event.target.scrollBy(-300, 0)
+       }else{
+          event.target.scrollBy(300, 0)
+       }
+  })
 
 
-    })
+
+
+
+
+
+  const imgs = document.querySelector('#imgC')
+  const img = document.querySelectorAll('#imgC .slide')
+  
+  let idx = 0
+  
+  function carrousel(){
+      idx++
+  
+      if(idx > img.length - 2){
+          idx = 0
+      }
+  
+      imgs.style.transform = `translateX(${-idx * 300}px)`
+  }
+  
+  setInterval(carrousel, 2000);
 
 
 }
